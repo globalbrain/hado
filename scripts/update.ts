254c57e3cf75e4d1e3c84effc113ad0a25a7f583
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2024-05-31 00:38:43 +0530

    chore: add deps update script

---
+++ scripts/update.ts
@@ -0,0 +1,41 @@
+import { $ } from '@david/dax'
+import { parse, resolveLatestVersion, stringify } from '@molt/core'
+import { expandGlob } from '@std/fs'
+import { relative } from '@std/path'
+
+const denoJson = JSON.parse(await Deno.readTextFile('deno.json')) as { imports: Record<string, string> }
+const newImports = { ...denoJson.imports }
+
+for (const [key, value] of Object.entries(denoJson.imports)) {
+  if (!value.includes(':')) continue
+
+  const parsed = parse(value)
+  let rangeSpecifier: string | undefined = undefined
+
+  if (parsed.version && /^[~^]/.test(parsed.version)) {
+    rangeSpecifier = parsed.version[0]
+    parsed.version = parsed.version.slice(1)
+  }
+
+  const resolved = await resolveLatestVersion(parsed)
+  if (!resolved) {
+    console.log(`Could not resolve latest version for ${key} (${value})`)
+    continue
+  }
+
+  resolved.version = `${rangeSpecifier || ''}${resolved.version}`
+  newImports[key] = stringify(resolved)
+}
+
+denoJson.imports = newImports
+
+await Deno.writeTextFile('deno.json', JSON.stringify(denoJson, null, 2))
+await $.raw`deno fmt deno.json`.quiet()
+
+await Deno.remove('deno.lock')
+
+const files =
+  (await Array.fromAsync(expandGlob('**/*.ts', { root: Deno.cwd(), includeDirs: false, exclude: ['**/_*', '**/.*'] })))
+    .map((x) => relative(Deno.cwd(), x.path)).join(' ') // simplify when https://github.com/dsherret/dax/issues/251 is supported
+
+await $.raw`deno cache --reload --lock=deno.lock ${files}`
