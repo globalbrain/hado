91f44ea9015fd7bea2e729c813d1227011d113b0
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2024-09-16 11:46:33 +0530

    chore: bump deps

--- dev_deps.ts
+++ dev_deps.ts
@@ -1,4 +1,4 @@
-import { $ as _$, type $Type } from 'jsr:@david/dax@^0.41.0'
+import { $ as _$, type $Type } from 'jsr:@david/dax@^0.42.0'
 
 export { parseFromJson } from 'https://deno.land/x/import_map@v0.20.1/mod.ts'
 export {
@@ -10,17 +10,18 @@ export {
   type SelectOptions,
 } from 'jsr:@cliffy/prompt@^1.0.0-rc.5'
 export { as, ensure, is } from 'jsr:@core/unknownutil@^4.3.0'
-export { createGraph, load as loadGraph } from 'jsr:@deno/graph@^0.82.0'
-export type { DependencyJson, ResolvedDependency } from 'jsr:@deno/graph@^0.82.0/types'
-export { assertEquals, assertExists } from 'jsr:@std/assert@^1.0.3'
-export { parseArgs, Spinner } from 'jsr:@std/cli@^1.0.4'
-export { filterEntries } from 'jsr:@std/collections@^1.0.5'
-export { bold, cyan, dim, green, magenta } from 'jsr:@std/fmt@^1.0.1/colors'
-export { expandGlob } from 'jsr:@std/fs@^1.0.2'
-export { getAvailablePort } from 'jsr:@std/net@^1.0.1/get-available-port'
-export { dirname, fromFileUrl, relative, resolve, toFileUrl } from 'jsr:@std/path@^1.0.3'
+export { createGraph, load as loadGraph } from 'jsr:@deno/graph@^0.82.1'
+export type { DependencyJson, ResolvedDependency } from 'jsr:@deno/graph@^0.82.1/types'
+export { assertEquals, assertExists } from 'jsr:@std/assert@^1.0.5'
+export { parseArgs } from 'jsr:@std/cli@^1.0.6'
+export { Spinner } from 'jsr:@std/cli@^1.0.6/unstable-spinner'
+export { filterEntries } from 'jsr:@std/collections@^1.0.6'
+export { bold, cyan, dim, green, magenta } from 'jsr:@std/fmt@^1.0.2/colors'
+export { expandGlob } from 'jsr:@std/fs@^1.0.3'
+export { getAvailablePort } from 'jsr:@std/net@^1.0.4/get-available-port'
+export { dirname, fromFileUrl, relative, resolve, toFileUrl } from 'jsr:@std/path@^1.0.6'
 export { escape } from 'jsr:@std/regexp@^1.0.0'
-export * as SemVer from 'jsr:@std/semver@^1.0.2'
+export * as SemVer from 'jsr:@std/semver@^1.0.3'
 
 export const $ = new Proxy(_$, {
   apply(target, thisArg, args: Parameters<$Type>) {
