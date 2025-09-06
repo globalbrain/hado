7b10761ef5cd33b41e62181a01c4757fd0b54b49
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2025-09-06 17:49:41 +0530

    chore: bump deps

--- src/router.ts
+++ src/router.ts
@@ -24,8 +24,8 @@ import { debounce } from 'jsr:@std/async@^1.0.14/debounce'
 import { walk } from 'jsr:@std/fs@^1.0.19/walk'
 import { serveDir, type ServeDirOptions } from 'jsr:@std/http@^1.0.20/file-server'
 import { STATUS_CODE, STATUS_TEXT, type StatusCode } from 'jsr:@std/http@^1.0.20/status'
-import { normalize as posixNormalize } from 'jsr:@std/path@^1.1.1/posix/normalize'
-import { toFileUrl } from 'jsr:@std/path@^1.1.1/to-file-url'
+import { normalize as posixNormalize } from 'jsr:@std/path@^1.1.2/posix/normalize'
+import { toFileUrl } from 'jsr:@std/path@^1.1.2/to-file-url'
 
 const methods = new Set(['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'PATCH'])
 const ignore = /^(?:.*?\/)?(?:(?:_|\.|node_modules\/|coverage\/).*|.*\.d\.ts)$/
