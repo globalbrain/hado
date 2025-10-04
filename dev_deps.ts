dc3b494d9c5b5643e06046911dc037faba111edc
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2025-10-04 16:23:38 +0530

    chore: log commands before running, switch to jsr version of import_map, bump deps

--- dev_deps.ts
+++ dev_deps.ts
@@ -1,11 +1,12 @@
 import { $ as _$, type $Type } from 'jsr:@david/dax@^0.43.2'
+import { gray } from 'jsr:@std/fmt@^1.0.8/colors'
 
-export { parseFromJson } from 'https://deno.land/x/import_map@v0.23.0/mod.ts'
+export { parseFromJson } from 'jsr:@brc-dd/import-map@^0.24.0'
 export { Confirm, Input, Select } from 'jsr:@cliffy/prompt@^1.0.0-rc.8'
 export type { ConfirmOptions, InputOptions, SelectOptions } from 'jsr:@cliffy/prompt@^1.0.0-rc.8'
 export { as, ensure, is } from 'jsr:@core/unknownutil@^4.3.0'
-export { createGraph, load as loadGraph } from 'jsr:@deno/graph@^0.101.0'
-export type { DependencyJson, ResolvedDependency } from 'jsr:@deno/graph@^0.101.0/types'
+export { createGraph, load as loadGraph } from 'jsr:@deno/graph@^0.102.0'
+export type { DependencyJson, ResolvedDependency } from 'jsr:@deno/graph@^0.102.0/types'
 export { denoPlugins } from 'jsr:@luca/esbuild-deno-loader@^0.11.1'
 export { assert, assertEquals, assertExists, assertInstanceOf } from 'jsr:@std/assert@^1.0.14'
 export { parseArgs } from 'jsr:@std/cli@^1.0.22'
@@ -20,8 +21,15 @@ export { build } from 'npm:esbuild@0.25.5'
 export { default as tsid } from 'npm:unplugin-isolated-decl@^0.15.2/esbuild'
 export { z } from 'npm:zod@4.1.11'
 
+const enc = new TextEncoder()
+
 export const $ = new Proxy(_$, {
   apply(target, thisArg, args: Parameters<$Type>) {
+    Deno.stderr.writeSync(enc.encode(gray(`$ ${compileTemplate(...args)}\n`)))
     return Reflect.apply(target.raw, thisArg, args).quiet()
   },
 })
+
+function compileTemplate(strings: TemplateStringsArray, ...exprs: unknown[]): string {
+  return strings.reduce((acc, str, i) => acc + str + (i < exprs.length ? String(exprs[i]) : ''), '')
+}
